3893696338160907608222dab5f5acfec600c7da
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-12-18 07:42:23 +0800

    React Node + Express Router

---
+++ backend/routes/booksRoute.js
@@ -0,0 +1,109 @@
+import express from 'express';
+import { Book } from '../models/bookModel.js';
+
+const router = express.Router();
+
+//route to save new book
+router.post('/', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publish Year'
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message})
+    }
+})
+
+// Route to Get all books in Database
+router.get('/', async (request, response) => {
+    try {
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Route to Get one books in Database by id
+router.get('/:id', async (request, response) => {
+    try {
+
+        const { id } = request.params;
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+// Route to update a Book
+router.put('/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publish Year',
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' });
+        }
+
+        return response.status(200).send({ message: 'Book updated succesfully' });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
+
+// Route to Delete a book
+router.delete('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found'});
+        }
+
+        return response.status(200).send({ message: 'Book deleted succesfully'});
+
+    } catch (error) {
+        console.log(eror.message);
+        response.status(500).send({ message: 'Book deleted succesfully'});
+    }
+});
+
+export default router;
