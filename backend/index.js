ac7b83809fb4f0321c092eaf07f44911f490edf6
Muhammad Sufi Adam Bin Samsuhaimi <74510342+Adamlamia@users.noreply.github.com>
2023-12-18 00:45:46 +0800

    Install Modules
    finish post and get books

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ import { Book } from "./models/bookModel.js";
 
 const app = express();
 
+// Middleware to parsing request body
+app.use(express.json());
+
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('MERN Stack');
@@ -36,6 +39,21 @@ app.post('/books', async (request, response) => {
         response.status(500).send({ message: error.message})
     }
 })
+
+// Route to Get all books in Database
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
 
 mongoose
     .connect(mongoDBURL)
